83b257b9b90dd307ca6e991a76fe3e84965a276d
chriswongatcuhk <wc38.wong@gmail.com>
2017-01-30 12:18:16 +0800

    lodimob java solution.

--- java/Xbonacci.java
+++ java/Xbonacci.java
@@ -3,13 +3,10 @@ import java.util.Arrays;
 public class Xbonacci {
 
 	public double[] xbonacci(double[] signature, int n) {
-		double[] result = Arrays.copyOf(signature, n);
+		double[] x = Arrays.copyOf(signature, n);
 		for (int i = signature.length; i < n; i++) {
-			result[i] = 0;
-			for(int j = 1; j <= signature.length; j++){
-				result[i] += result[i - j];
-			}
+			x[i] = Arrays.stream(x).skip(i - signature.length).sum();
 		}
-		return result;
+		return x;
 	}
 }
